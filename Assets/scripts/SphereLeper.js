00df2bb886160fe025dbee5c8d79cc0c5bc22ced
Pratik Parija <pratik_parija@yahoo.com>
2015-03-31 14:58:47 -0400

    initial transfer test

---
+++ Assets/scripts/SphereLeper.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+function Start () {
+	
+}
+
+function Update () {
+	if(Input.anyKeyDown){
+		MoveOver;
+	}
+}
+
+function MoveOver(){
+	var t:float = 0;
+	var startPoint = Vector3(-5, 0, 0);
+	var endPointt = Vector3(5, 0, 0);
+	
+	while(t<1){
+		transform.position = startPoint*(1 - Mathf.Pow(t, 2))+endPoint*Mathf;
+		t += 0.1 * Time.deltaTime;
+		yield;
+	}
+}
